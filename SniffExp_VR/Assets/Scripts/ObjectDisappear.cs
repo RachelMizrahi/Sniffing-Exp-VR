11f92b006e4db31331c45a04ce6f712bbe2e3eb4
RachelMizrahi <mizrrac@post.bgu.ac.il>
2025-01-26 18:13:29 +0200

    c

--- SniffExp_VR/Assets/Scripts/ObjectDisappear.cs
+++ SniffExp_VR/Assets/Scripts/ObjectDisappear.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class ObjectDisappear : MonoBehaviour
 {
-    public GameObject objectToDisappear;  // The object that will disappear
-    public string[] triggerTags;  // Tags for areas that trigger the object disappearance
+    // Reference to the audio source component
+    private AudioSource audioSource;
 
-    // Called when another collider enters the trigger area
-    private void OnTriggerEnter(Collider other)
+    // Reference to the prefab of the object that will be shown next
+    public GameObject objectPrefab;
+
+    // The position on the table where the object should appear
+    public Transform appearPosition;
+    
+    // This function is called when the object enters a trigger collider
+    void Start()
     {
-        // Check if the collider that entered has one of the tags specified in triggerTags
-        foreach (string tag in triggerTags)
-        {
-            if (other.CompareTag(tag))  // Compare the collider's tag
-            {
-                // If the object to disappear is assigned, deactivate it
-                if (objectToDisappear != null)
-                {
-                    objectToDisappear.SetActive(false);  // Disable the object
-                }
-            }
-        }
+        // Get the audio source component attached to the object
+        audioSource = GetComponent<AudioSource>();
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object collided with a collider that has the tad "Answer Collider"
+        if (other.CompareTag("Answer Collider"))
+        {
+            // Play the sound
+            if (audioSource != null)
+            {
+                audioSource.Play();
+
+            }
+
+            // If the object enters the trigger area, destroy it
+            Destroy(gameObject);
+
+            // Instantiate a new object on the table at the specific apear position
+            Instantiate(objectPrefab, appearPosition.position, Quaternion.identity);
+        }
+                
+    }
+
 }
